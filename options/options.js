2cf666a4375405fc17ff17849006fd217af904ca
hi bud i yeeyee hi!!! <clewis198431@gmail.com>
2023-10-12 20:02:47 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "minecraft-mod-maker-43909",
-  url: "https://minecraft-mod-maker-43909.botics.co",
-  api: "https://minecraft-mod-maker-43909.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "minecraft-mod-maker-43909",
+  "url": "https://minecraft-mod-maker-43909.botics.co",
+  "api": "https://minecraft-mod-maker-43909.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "AddonMaker"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
