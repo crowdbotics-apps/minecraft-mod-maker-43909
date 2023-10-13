6016fc54f192eccc50c84e5aee04a7bac097e75f
hi bud i yeeyee hi!!! <clewis198431@gmail.com>
2023-10-12 20:05:53 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/AddonMaker/index.js
+++ screens/AddonMaker/index.js
@@ -1,11 +1,22 @@
+import { useSelector } from "react-redux";
+import { rest_auth_user_retrieve } from "../../store/minecraftmodmakerAPI/userDetails.slice.js";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import React from "react";
 import { SafeAreaView, View, Text, StyleSheet, Button } from "react-native";
 
 const MinecraftWeaponMaker = () => {
+  const {
+    entities: Signups
+  } = useSelector(state => state.Signups);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(rest_auth_user_retrieve());
+  }, []);
   return <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Minecraft Weapon Maker</Text>
       <View style={styles.buttonContainer}>
-        <Button color="yellow" title="Create Weapon" onPress={() => {}} />
+        <Button color="yellow" title="Create Weapon" onPress={() => {}} data={Signups} />
       </View>
     </SafeAreaView>;
 };
